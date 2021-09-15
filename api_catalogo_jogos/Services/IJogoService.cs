db278ad1ab56638aaf6d3b95745f0fb8568c793b
lucianapacheco <lucianapachecomelo@gmail.com>
2021-09-14 21:40:47 -0300

    controller

---
+++ api_catalogo_jogos/Services/IJogoService.cs
@@ -0,0 +1,18 @@
+﻿using api_catalogo_jogos.InputModel;
+using api_catalogo_jogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace api_catalogo_jogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid id, JogoInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+    }
+}
